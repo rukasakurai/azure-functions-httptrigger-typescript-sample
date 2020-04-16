66b9d9fbf8dda82b94ee1b15cf4431835bf86d73
Ruka Sakurai <ruka.sakurai@sakuraierikonoMacBook-Pro.local>
2020-04-16 12:18:28 +0900

    Adding jest unit tests

--- SampleHttpTrigger/index.ts
+++ SampleHttpTrigger/index.ts
@@ -4,17 +4,23 @@ const httpTrigger: AzureFunction = async function (context: Context, req: HttpRe
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
 
+
     if (name) {
+        const message:String = "Hello " + (req.query.name || req.body.name);
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
+            body: message
         };
+        console.log(`message displayed: ${message}`);
+        context.log.verbose(`message displayed: ${message}`);
     }
     else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
+        console.log('name was not passed in the http request');      
+        context.log.warn('name was not passed in the http request');
     }
 };
 
---
+++ test/SampleHttpTrigger/index.spec.ts
@@ -0,0 +1,26 @@
+import { mock } from 'jest-mock-extended';
+import { Context } from '@azure/functions';
+import httpTrigger from "../../SampleHttpTrigger";
+
+const mockContext = mock<Context>();
+
+test('"World" is passed as a name', async () => {
+    const mockReq = {
+        query: {
+            name: 'World',
+        },
+    };
+
+    await httpTrigger(mockContext, mockReq);
+    expect(1).toEqual(1); // TODO: Add meaningful assertion
+});
+
+test('No name is passed in http request', async () => {
+    const mockReq = {
+        query: {
+        },
+    };
+
+    await httpTrigger(mockContext, mockReq);
+    expect(1).toEqual(1); // TODO: Add meaningful assertion
+});
